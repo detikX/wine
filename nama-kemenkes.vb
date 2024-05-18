74e26e990882ff76944abf0a4762f5b197a50cc8
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-05-19 06:17:24 +0700

    frfrf

---
+++ nama-kemenkes.vb
@@ -0,0 +1,502 @@
+AGUS SALIM
+ Retno Ambarwati SKM, MKM
+Santi Rahayu S.Kom
+dr Tia Mardiyah Arifin
+Junita Manurung Amd. Kep.
+Teguh Dwi Riyanto S.Ikom
+Edi Pranoto
+PATRICIA FLORENCE WALLAH 
+Teguh Dwi Riyanto S.Ikom
+Endro Muljandari S. Kep
+Erick Yudo Baskoro SH, MM
+Rr. Endah Khristanti WW SKM, MKM
+Abdul aziz
+dr Maria Siska Mudina
+dr dewi indra sari 0
+Egi Kurnianto SE
+dr. Nila Mulyani M.Si
+. Tumpal P Hendriyanto S.Kom, MSc(PH)
+ HERI PURWANTO 
+Bambang Suroso SP
+SURIYAT
+ HERI PURWANTO 
+Abdul Abdul aziz Aziz
+ Musjaya Bin Muhamad 
+ Muhamad 
+Sunarto
+0 Martinella LRS SS
+Zakaria SKM, M.Kes
+Endro Muljandari S. Kep
+Hera Setiawan ST
+Tania Agustina 
+dr Dewi Syafrina Nasution
+Drg Juncai Atmadja
+drg Febrina Dumaria Pardede MKM
+Fatimah Fajrin S.Sy
+Euis Nina Wardini SE
+Dora Handyka SST, MKM
+Farida Ratnasari
+Farhan Yugarpaksi S.Pd
+dr.  Sri Wahyuni M.Sc
+Drs Teguh Sarwono MM
+Yoyok Hari Saputro ST
+Dwi Retno Wijayanti SKM
+Yulius Handoko
+HERLAMBANG SKM,MAP
+Putut Supawolo S.Pd
+ Mei Suryani
+dr Maria Siska Mudina
+Meilina Suwandi SKM
+Putri Nur Hanifah SKM, M.K.M
+Febri Syahida SKM, MKM
+ Yuyun Widyaningsih  SKp. MKM
+Intan Agusti Permatasari SE
+BAN HAJi SE
+Sonya Shobrina Ns, S.Kep
+YETTY AZRIANI DCN, MPH
+Indriaswari Widhakuswara  SKM
+dr. Dimar Kencono S.Ked
+Muh. Kholilurrohman Amd. Kom
+Raden Raditya Utama S.Kom
+Sumono
+Tarsinah SE
+Dr Darmawan M.Kes 
+Wenny Christian kosong
+Muhajar
+Yusup
+Hani Annadoroh SKM, MKM
+Kandar SE
+Sigit Pramono
+Nia kurniasih Amg
+Hertina Jatnika Putra SST, MM
+M. Sugiyanto
+  Hayatul Rahmi SKM
+Maryani SKM
+  Dhea Adinda Ramadhan SKM
+WASETYO Tidak ada
+suroto Amd
+Kartinah
+Elvira Rahma Farisa S. Pd
+Mohammad Yani S.T., M.Tr.A.P.
+dr Indriya Purnamasari MARS
+drg Musliana
+Adhi prasetyawan SE
+drg Ni Ketut Widyaningsih MKM
+Lita Noviyanti  A.md
+drg Bushrah Tahirah
+Muhamad Lutpi Fariddudin S.Tr.AP
+Jati Setyo Dwi Susanti SE
+Roseria Yulianto SKM
+drg LINDA SUSANTI MKM
+Dian Arief Hawindati  SKM.MPd
+apt Dena Adiestia S. Farm
+R. R. Kuswardhani  SH, M. AP
+Dr Fitria MKM
+Antonius I.P Saputro S.K.M
+Andri Widayati SKM
+Sitti Nuraini SKM, MKM
+Teguh Supriyadi
+drg Leni Kuswandari MKM
+SITI HAJAR SKM, M.KES
+NOPIANTO RICAESAR S.IKom, M.K.M
+Agus Dwiyanti S. K. M
+Rizki Aldiansyah 
+Rima Nestiningtyas  A.Md
+Andriyatno Tidak ada
+Dewi Roro Kumbini S.Pd, MKM
+Widi Astuti  ST
+Bejo Sugiarto
+Aswita Bagiawati Tidak ada
+Drs I Dewa Gede Astika 
+ATIK PURWANTI SKM
+Dian Rahayu Pamungkas SKM, MKM
+Gumilar Sastra Dipura ST
+dr Sari Hayuningtyas MKM
+Diani Purwitasari S.Sos
+drg Ilham Chandra MARS
+Ida ayu agung Mardiani putri S.Kom. MKM
+Aa Tes nama Tes nama
+Lupi Trijayanti SKM
+Januar Rimuda SKM
+Alif Insan Al Farisi S.K.M.
+Simanjuntak Agustina SKM, MKM
+Lies Setyowati SE
+Imam   
+Ns. Ifrani Yuan Khaulaharani  S.Kep
+Meida Atik Nazihah A.Md
+Hilman farras S.Kom
+AFIFAH KANIA DEWI S.ST
+Farah Alya Nurani S.Tr.Kes
+Rian Chaidir Hidayat A.Md
+Neneng Saadah
+Dwi Herryanto S.Kom
+Akemat SKp, MKes
+Aswita Bagiawati
+Dinnia Hanivah Novasari SKM
+Aldila Riznawati S.Kom, MKM
+Yopita Ratnasari  SST, MKM
+Deri Pinesti SKM, MKM
+Iyan Tasnaya 
+Wisca Gumanti SN S.Psi
+Afriani Tinurbaya S.Kep., Ns., MKM
+TEZA YUNANDA EVRIZA
+Eric Irawati S.Si.T, MKM
+ Muradih  
+Rasimin 
+Lycia Pricia A.Md
+Hayatul Rahmi SKM
+Ns Yudi Mulyana S.Kep
+Nurul Widya Susanti SH, M.Sc
+Tjuk Wibi Pratikto S.Kom
+Hemi Prasetyaningrum  S.H
+Heropati Zhahara A.Md
+Nia Fitriasari  SST, M.K.M.
+Yuyun Isnu Rohaenah S.TP
+HARYATI SKM.M.Pd.
+Wenny Christian
+Nurul Fajriani S.M
+Amanda Widlanisia SKM
+Ilham Rabbye Ghummaiisha A.Md.S.I
+Edi Pranoto
+Anwar rahman
+Ariw Ayudya Novinier S. Sos, MKM
+Mentari Evarani S.Tr.Keb
+Rini Susanti S.Psi
+Kasmuri SAP
+Novalia Tri Dara S.E.
+Intan Agusti Permatasari SE
+Arif Budiyanto A.md
+Nia kurniasih Amg
+Citra miranti
+Rohman Rohim SE
+Ermita Isnaeni Putri Susyanti S.KM
+Farah Amalia Putri SKM
+Rima Novriyanti  S.Akt, M.M
+Prima Dina Atika S.Hum
+Heni Dianawatisari SKM
+Uray Salawati SIK,M.Si
+Abbiyyu Raihan Ramadhan SH
+Made Sudiartati
+Ira Herawati S.Kep., Ns
+Robbiatul Afda'tiyah SKM, MKM
+Rizqi Adhi Nugroho SE
+Imam Wahyudi ST, M.K.M
+  Lela Ayu Widayanti A.Md.Ak
+Fintriane Gilda S.Kom, MKM.
+ Umi Hidayati SE
+Erni Endah Sulistioratih SKM, M.Erg.
+Khairun Insani SKM
+Iqbal Ramadhan S.E
+Esde Dianusana Etieka S.Farm., Apt., MKM
+Ayu Citra Wangsanita S.Kom, MKM
+Putut Supawolo S.Pd
+SKM Maryani
+Dedy Suryadi SST, SAP, MM
+drg Siti Nurbudiastuti MKM
+Sumono
+Yulia Annisa S.Hum
+Rosi Mufti Girinda S.Gz
+Yulita Rizki Prawidyanti S. Kep. 
+Anwarul Haq A.Md
+Ibu Oom Komalasari M.Kep
+Ns Rosmalinda S.Kep
+Nevilia Lybrani Dwiputri Detaq SE
+SRI HARTONO SE
+Hermawan Akbar 
+Erick Yudo Baskoro SH, MM
+Soni Hartono A.Md
+Farid Kharisma S.A.P
+Nur Afifah Kurniati S.Sos, MKM
+Ahmad Afzal Alkhatami Addin BBA, S.Kom
+DEA NIDYA GANATIKA S.H
+Erni Saptiani  SST., MKM
+Arrum Shafa Maulidiazmi Umar, S.K.L S.K.L
+ ANJUNG TRISNAWATI A.Md
+Nur Kholish Majid S.Kep
+dr. Novriani
+Aditya Dwi Purwaningsih SKM
+Jai Janudin
+drg MADE MURYANI TRININGSIH MA
+Mohamad Syachroni S.Kom
+Fitria Ariyani SE., MA.
+Fitria Kusuma Ratih SH, MKM
+Raissa Nabila Putri Endika S.Tr.Kes
+Agus Yuliantoro 
+drg Dian muliawati MPH
+Hendra S.Kom
+SRI HARTONO SE
+dr Khairunnisa
+Sigit Dwi saputro  S.Kom
+Emyl Fahmy
+0 Martinella LRS  SS
+Suryawati Endaningsih Purba SKM, MKM
+Lenny Agustaria Banjarnahor SSt. M.Fis
+Ratia Radiani SST. MKM
+Sri Widaningsih Tidak ada
+SAHRUL RIJAL S. M
+Ns. Ifrani Yuan Khaulaharani  S.Kep
+Yesi Anggra Dewi SKM
+Chaerudin SE
+Litriane Rispa Widhy SKM
+dr. Jefri Thomas Alpha Edison MKM
+Henriette Nuarni Sari SKM., MKM.
+Banyu Herdiansyah S. Kom
+Siti Nur Khaerani A.Md.Kep
+SKM Solihin S.K.M
+Harjaka 
+JULIANTY
+dr. Dian Ramadhani
+dr Maria Siska Mudina
+Septi Rizqi Ramadhanti S.K.M.
+Elis Mulyati S.S.T., M.Keb
+Pasmawati SKM, M.Epid
+Rina Nurseto SE
+dr Nurrahmiati MKM
+SEPTY ENDAH RAHAYU SE, M.A.P
+Syarifah Khodijah  S.K.M 
+Aminah
+dr dewi indra sari
+TRI HERLISTIADI Amd
+ Ujang Abdul Rohman 
+Ibnu Dwi Hartanto SE
+TUTUT WAHYUGUNAWAN S.Kom
+Ns Dian Pancaningrum S.Kep, M.Kep
+Fitri Marantika SE
+Umar S.Sos
+Lutfi Andriawan S.A.B.
+Sifa Fauziah S.E
+S.M Nurhayati, S.M S.M
+Endang Prihayuni S.Sos
+Heri Sudrajat 
+Dedy Suryadi SST, SAP, MM
+Fiska aprilia SKM
+Indah Sulistyanti SKM
+MR YOSIAS.TIAUMESA TIAUMESA
+Fitria Ariyani SE., MA.
+FERNIA PARAMITHA SKM
+Rr. Endah Khristanti WW SKM, MKM
+Ismawiningsih SKM.MKM
+dr Nila Mulyani M.Si
+Andrie Purnomo S1
+Susanti SE, MM
+Desi Aprianti S.E
+Syarifuddin A.Md.Ak
+dr Ita Dahlia MH
+Ns. M. Irsyad Halim S.Kep
+Ns Ifrani Yuan Khaulaharani  S.Kep
+Ajeng Retno Yunita SKM
+Reza Gustina Annisa,  A.Md.Ak
+Drs IDewa Gede Astika 
+Marini Panggabean SH
+Irma Septianah A.Md
+Dhea Adinda Ramadhan SKM
+Deden Kusnadi
+Supriyanto
+Peri Padli Saputra S.Kep., Ns
+ BARDANOVA LIBRIANSA SH
+Sindhyastuty Krisnamurti S.Kom
+PATRICIA FLORENCE WALLAH 
+ siswoyo 
+BERNADUS OKTAVIAN HARTANTYO S.E
+Kuncoro Yudhianto S.Kom
+Ahmad Fauji
+Amylia Nuzidati ST
+Erwin Supari Simanjuntak SE
+Hafrizal S.Kom
+Ias Tarina Puspitasari SKM, MKM
+Titiek Rahmawati A.Md
+Jhovia Aloedya Pramana SKM
+YULIUS HANDOKO
+BOBBY SURYO WIBOWO SS
+Yennidar  S.AP, MKM
+Kharisma Sari ST
+Putri Nur Hanifah SKM, M.K.M
+ Tuti Lestari
+Ari Sandi
+Atik Purwanti SKM
+Regita Novianti S.Sos
+Chaira Wildha ST, MAP
+Ahmad Joko Purwanto 
+Arsiparis BUDI Arsiparis
+tidak da Arief Widjaya SST, MKM
+KARPI
+Aminah
+Mulyadi  SE
+drg Erly Novita
+Elia Tri Rahayu 
+  Kumbara Jaya S. Kom
+Gemasih Pintanine S.Sos, MKM
+  Andrie Purnomo S1
+Siti Hajar SKM, M.Kes
+ Rianto 
+Teza Yunanda Evriza
+Sopyan Haji S.T
+drg Irma Yuliana
+Ratna Ayu Istiani 
+Anwar Rahman
+Meila Kushendiati SKM,MKM
+Muhamad zulham 
+Asri Dyah Noernaningroem SE
+Nadya Yuliana S.I.Kom
+Patriyah S.Kom
+drg Alia Nutria 
+Riani Lisgianti Pratiwi S.M
+Ns. Gistaria Fani Stendiyanti S.Kep
+Asri Dyah Noernaningroem SE
+Moch Aziz Arindya Mukti S.Pi
+Namira Rahmi Zahara S.AP, M.Hum
+Nofrita Ayu S.Tr.KL
+ASYIFA HERSUMULYOASTUTI S.KOM
+Yudith Tammy Mawey SH
+Afifah Kania Dewi S.ST
+Sri hartono SE
+Nur Azizah A.Md
+subekti
+Vermona Marbun. Skp.MKM
+Sunarto
+Saurikah SKM
+Murwaniyah S.Sos
+Anida Fathiyah Mutiatunnisa SKM
+Faijah SE
+Heni Dianawatisari SKM
+Yun Inayani Satari SE., MM., Akt
+Soni Hartono A.Md
+Rispan 
+  Masrulloh SKM, MKM
+Najmi Firdaus  S.Sos.
+IWAN IWAN SETIA NURSIDIK
+Desy Apriana
+Meilina Suwandi SKM
+Hary Brimajaya Iswandy S.H
+      Nurdiansyah SE
+Agustina Simanjuntak SKM, MKM
+Yanuardo Ganda Drabenzus ST, M.Pd
+sj Dody Nurzaman Anwar SH
+Amalia Nur Ainina S.Tr.Gz
+Riezka Dwi Pratiwi S.H
+0 Endang Suhartini SKM, MM
+Sri Denti On Madya Purba SKM,MKM
+Lufthans Arstipendy S.I.Kom, M.Sc.GUH
+M. Teguh Pribadi
+Rita Apriani
+ ARI SUJATMIKO AMK 
+Robertus krismantoro
+Sifa Fauziah
+Dimas Kurniawan A.Md
+yuni pristanti S.Kom
+Maretta Monalisa S.K.M
+  Ahmad Maulanal Hakim Amd
+  Cyndy Stevany Sape SKM
+Novita Widyasputri S. Ik
+Hary Sukoco SE
+Ari Hartanto SE
+  Sarju Siswoko SE
+Nur Afidah S.KM
+Mr. KUN PRAMBUDI SE, MM
+Ramlah Amalia Wathi ST
+Pratiwi Febrianti SE
+  Zaki Fauzan Harahap  
+Ratna Yustinawati  
+Rinvilda Aninditya S.T
+  Siti Muslifah A.Md
+. Barlian Pandu Pratama SE
+Rodger Newman SM
+Noor Farida  SKM
+Faryan Dwi Satriyo
+  Andrie Purnomo S1
+  Dewi Tri Nugraheni  S.Sn, MKM
+Bayu Wijayanto SH.,MH
+NURDIN
+  Sugiarto SKM,MKM
+ADE NOVIANA AZHAR S.Ak
+Jumaina SKM, MKM
+Heru Kuswanto S.Kom
+Drs Teguh Sarwono MM
+Anita Novianty S.Sos
+Fika Amelia A.Md
+Rizdiana Widi Pratiwi S.E
+Yes Firmansyah Yes
+Ob Ahman
+Sifa Fauziah S.E
+  Agus Kurniawan S.I.Kom
+Dhanang Yudha Swara SH
+Sartika S.Kom
+Pawit Soegiharto  SE, MM
+Mansyur Iskandar Tidak ada
+Erni Endah Sulistioratih SKM, M.Erg.
+  MOHAMAD ROYANI SE
+Bayu Sukoco Putra S. Psi
+Radinal Husein SKM, MKM
+Wini Wulansari S.Si, MKM
+Donna Frediska Pandiangan SKM, MKM
+Nurohman 
+Drg Wahyu Nugroho MPH
+Ns.  Izana Anggriani S.Kep, MPH
+drg Irma Yuliana
+Herawati S.E
+Laila Nur Rokhmah SKM, MKM
+Dara Puspita Lestari S. KM
+Rika Mudrikah SKM
+Dwi Nurokhmatin M. S.I.P, M.Tr.AP
+Chaerudin SE 
+Sunarso SE
+Retno Prihastuti SE
+Ns. Wahyu Purnomo  S.Kep, MM
+Eroh Maesaroh SE
+Teguh Dwi Riyanto S.Ikom
+YUNI SARI ROMADHONA SKM, MKM
+Santi Rahayu S.Kom
+STANDFORD JAAN FIETER MARTHIN S.Akun
+IRMA SEPTIANAH A.Md
+Pradilah Ramadhan Tidak ada
+  Raras Purna Enrilladela SKM
+Muhammad Rifki Chamim A.Md.Ak.
+Mawaddatun Nadiyah SKM
+Kusmiyati
+Vita Andriani S.H
+Nurbani Kumala Sari SE
+Puspa Indah Budiono SKM, MKM
+Katarina Widiaswati ST
+Mira Weningtyas Mutiara S.E
+Alfinda Jevi Veradikawati
+DR Solihin SKM
+ Wali yadi 
+dr. Fetty Ismandari  M. Epid
+Juwarso 
+Rika Lestari SE
+Drg. Angger Rina Widowati MKM
+Maritania  S.H., M.H.
+SUDARTONO SH
+Wita Yulianti Makki SE
+Oni Submarwati S.Sos, M.SI
+Novica Mutiara R SH
+ YOSEP  
+Deddy Herdian  S.Kom
+Abdurrasyid dzaki tsaqofi S.Ak
+Wasan
+dr Liliek Sulistyowardani MKM
+  Lestari SKM, M.Si
+Sunarso SE
+Sardiya 
+Irma Fitriyana Herman S.Sos, MKM
+Nurfadly Khusnanto SH
+Sugiharto SKM, MM, MKM
+Ns.  Oktavian Megawati S. Kep
+0 Rumia Nurul Aeni SH.
+Des Fadli Firdaus S.E.
+Ariestya Anggraeni S.K.M, M.K.M
+MARKUS S.Kom, MKM
+dr. Dian Ramadhani
+Dadam kodar muslihat ,
+dr. Dian Ramadhani
+Mohammad Hilman Mursalat S.H
+Haryadi
+dr. Henny Erlina Aritonang 
+   Sandi susworo  
+Justine Saras Sita S.Ak.
+Junita Manurung Amd. Kep.
+drg Agusti Medika Putri
+FAIZAL
+Lilis dwi kristyaningrum SS
+Dendy Pratama Tidak ada
+Rismannidar S.Kom
