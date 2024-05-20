fc2b9ef5ca159012441c3ba2d160b5314c03c881
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-05-20 12:54:39 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+$(document).ready(function () {
+    AOS.init()
+})
 $('.slider-bukti').slick({
     dots: true,
     infinite: true,
@@ -5,3 +8,7 @@ $('.slider-bukti').slick({
     fade: true,
     cssEase: 'linear'
 });
+
+
+$(".data .wrap-detikx p").attr({ "data-aos": "fade-in", "data-aos-delay": "200", "data-aos-duration": "300" });
+$(".data .topx").attr({ "data-aos": "fade-left", "data-aos-delay": "2500", "data-aos-duration": "500" });
